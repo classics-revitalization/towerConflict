51f92170034d85e8f9c401234d2e35c1fd416a94
boshj8 <49296901+boshj8@users.noreply.github.com>
2021-04-08 21:53:55 -0500

    Fixed crash issue with script
    still working on camera/movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,38 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	
-	private string moveInputAxis = "Vertical";
-	private string turnInputAxis = "Horizontal";
-	
-	//degrees per second for rotation
-	public float rotationRate = 360;
-	
-	public float moveSpeed = 2;
-	
+
+    private string moveInputAxis = "Vertical";
+    private string turnInputAxis = "Horizontal";
+
+    //degrees per second for rotation
+    public float rotationRate = 360;
+
+    public float moveSpeed = 2;
+
     // Update is called once per frame
     void Update()
     {
         float moveAxis = Input.GetAxis(moveInputAxis);
         float turnAxis = Input.GetAxis(turnInputAxis);
-        
+
         ApplyInput(moveAxis, turnAxis);
     }
-    
+
     private void ApplyInput(float moveInput, float turnInput)
     {
-    	Move(moveInput);
-    	Turn(turnInput);
+        Move(moveInput);
+        Turn(turnInput);
     }
-    
-    private void Move(float input) 
+
+    private void Move(float input)
     {
-    	transform.Translate(Vector3.forward * input * moveSpeed);
+        transform.Translate(Vector3.forward * input * moveSpeed);
     }
-    
-    private void Turn(float input) 
+
+    private void Turn(float input)
     {
-    	transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+        transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
     }
-    
+
 }
